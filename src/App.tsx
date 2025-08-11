d7ade56e22f77169cd27c24fe9e4fbabd62fc5b9
leesm <ask135799@naver.com>
2025-08-12 01:31:35 +0900

    fix/과제페이지 monorepo 구성을 위한 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Work080701 from '@/modules/work080701';
-import Work080702 from '@/modules/work080702';
-
 import { Main } from './pages/Main';
 
 const App = () => {
@@ -13,8 +10,6 @@ const App = () => {
 				<div className='app-main'>
 					<Routes>
 						<Route path='/' element={<Main />} />
-						<Route path='/work080701' element={<Work080701 />} />
-						<Route path='/work080702' element={<Work080702 />} />
 					</Routes>
 				</div>
 			</div>
